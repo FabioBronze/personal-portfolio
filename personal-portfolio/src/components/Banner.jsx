cfa2c077d80c0d3e37ddf0d57d03ea0934fb3135
Fábio Bronze <fabio.r.bronze@hotmail.com>
2023-03-23 02:16:20 +0000

    Banner

--- personal-portfolio/src/components/Banner.jsx
+++ personal-portfolio/src/components/Banner.jsx
@@ -1,16 +1,61 @@
+// Bootstrap
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 
+// Hooks
+import { useState, useEffect } from "react";
+
+// Image
+import Header from "../assets/img/header.jpg";
+
 const Banner = () => {
+  const [loopNumber, setLoopNumber] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Fábio Bronze", "Front-End Developer"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNumber % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNumber(loopNumber + 1);
+      setDelta(500);
+    }
+  };
+
   return (
     <section className="banner" id="home">
       <Container>
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
-            <span className="tagline">Front-End Developer</span>
+            <span className="tagline">Hi! I'm</span>
             <h1>
-              {`Hi! I'm Fabio`}
-              <span classNamewrap>Web Developer</span>
+              <span classNamewrap>{text}</span>
               <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. At sed
                 voluptatum rerum porro dolorem eum aspernatur maxime officia,
@@ -18,12 +63,12 @@ const Banner = () => {
                 perferendis dicta veritatis adipisci ullam delectus.
               </p>
               <button onClick={() => console.log("connect")}>
-                Let's Connect
+                Download CV <ArrowRightCircle size={25} />
               </button>
             </h1>
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <img src="" alt="Background" />
+            <img src={Header} alt="Background" />
           </Col>
         </Row>
       </Container>
