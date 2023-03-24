d93fa06a2d1fec2f81db26cb57e078025ca1335b
Fábio Bronze <fabio.r.bronze@hotmail.com>
2023-03-24 19:44:31 +0000

    Skills

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -7,12 +7,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Components
 import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
