d19f35e6e0761aeac0cb1f70441fc0c4c669a0a9
Fábio Bronze <fabio.r.bronze@hotmail.com>
2023-03-27 03:45:45 +0100

    Deleted

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -9,7 +9,6 @@ import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -18,7 +17,6 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
-      <Contact />
     </div>
   );
 }
